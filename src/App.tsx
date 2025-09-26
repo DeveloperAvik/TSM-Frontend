d2a95ba8221681c7fe7a66c7d1b811a272068b83
root <avikd906@proton.me>
2025-09-26 19:40:32 +0530

    Configure react router for page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 
+import { Outlet } from 'react-router'
 import './App.css'
-import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <div className="flex min-h-svh">
-        <Button>Click me</Button>
-      </div>
+      <h1>This is App Component</h1>
+      <Outlet />
     </>
   )
 }
